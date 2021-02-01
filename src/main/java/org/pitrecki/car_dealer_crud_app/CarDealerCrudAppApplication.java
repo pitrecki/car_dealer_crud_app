78bdb593b7dabaf5dc001beec2f5de75acf611a1
pitrecki <piotr_nowak@outlook.com>
2021-02-01 18:56:58 +0100

    Initial spring like app structure

---
+++ src/main/java/org/pitrecki/car_dealer_crud_app/CarDealerCrudAppApplication.java
@@ -0,0 +1,13 @@
+package org.pitrecki.car_dealer_crud_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealerCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDealerCrudAppApplication.class, args);
+	}
+
+}
